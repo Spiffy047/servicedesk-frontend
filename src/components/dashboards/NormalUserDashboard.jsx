713163baa5831f600a3cc5406c9e6ddb35b797c3
Beatrice Kibor <kibor9346@gmail.com>
2025-10-24 17:51:26 +0300

    Add pulse animation to loading skeleton cards

--- src/components/dashboards/NormalUserDashboard.jsx
+++ src/components/dashboards/NormalUserDashboard.jsx
@@ -65,7 +65,7 @@ export default function NormalUserDashboard({ user }) {
           <div className="h-8 bg-gray-200 rounded w-1/4 mb-6"></div>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
             {[...Array(4)].map((_, i) => (
-              <div key={i} className="h-24 bg-gray-200 rounded"></div>
+              <div key={i} className="h-24 bg-gray-200 rounded animate-pulse"></div>
             ))}
           </div>
         </div>

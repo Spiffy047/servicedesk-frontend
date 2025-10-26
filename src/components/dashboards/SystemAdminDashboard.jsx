521c51d82652d1be8c4805736887b87f49b16cbc
Joe M Kariuki <mwanikijoe1@gmail.com>
2025-10-26 14:10:34 +0300

    admin panel delete

--- src/components/dashboards/SystemAdminDashboard.jsx
+++ src/components/dashboards/SystemAdminDashboard.jsx
@@ -232,11 +232,7 @@ export default function SystemAdminDashboard({ user, onLogout }) {
                           className="text-blue-600 hover:text-blue-800 mr-3"
                         >
                           Edit
-                        </button>
-                        <button
-                          onClick={() => handleDeleteUser(u.id)}
-                          className="text-red-600 hover:text-red-800"
-                        >
+                       
                           Delete
                         </button>
                       </td>

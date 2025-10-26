3109fbe86364c658b2e8c1ca29907221684a66b4
bethwelkipruto <bethwel.c7@gmail.com>
2025-10-26 12:29:35 +0300

    modified file

--- src/src/components/tickets/TicketDetailDialog.jsx
+++ src/src/components/tickets/TicketDetailDialog.jsx
@@ -21,11 +21,7 @@ export default function TicketDetailDialog({ ticket, onClose, currentUser, onUpd
     fetchAttachments()
   }, [ticket.id])
 
-  useEffect(() => {
-    if (scrollRef.current) {
-      scrollRef.current.scrollTop = scrollRef.current.scrollHeight
-    }
-  }, [messages, activities])
+  
 
   const fetchMessages = async () => {
     try {
